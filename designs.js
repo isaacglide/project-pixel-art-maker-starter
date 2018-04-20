366d106c65dca13f6a68ffb34861d1994361bc19
Isaac Glidewell <isaacglide@gmail.com>
2018-04-20 12:40:04 -0500

    adding start of javascript

--- designs.js
+++ designs.js
@@ -1,4 +1,12 @@
 // Select color input
+let colorPicking = document.querySelector('#colorPicker');
+let chosenColor = null;
+
+colorPicking.addEventListener('input',function (event) {
+  let chosenColor = event.target;
+});
+
+
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
